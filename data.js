803c0b07f806c61328f5fefa913e5820af30fe95
Jazmin Robles <jazmasoterapeuta@gamail.com>
2022-08-17 12:11:20 -0400

    Updates

---
+++ data.js
@@ -0,0 +1,31 @@
+const filterData = (characters, species) => {
+  let dataFiltered = characters.filter(function (character) {
+    return character.species === species;
+  });
+  return dataFiltered;
+};
+
+export const filtroGenero = (characters, gender) => {
+  let dataFilter = characters.filter(function (character) {
+    return character.gender === gender;
+  });
+  return dataFilter;
+};
+
+const sortData = (Data,sortBy) => {
+  let orderAZ = Data.sort(function (a, b) {
+    if (a[sortBy] > b[sortBy]) {
+      return 1;
+    }
+    if (a[sortBy] < b[sortBy]) {
+      return -1;
+    }
+    
+  })
+ return orderAZ;
+ 
+}
+
+
+export { sortData };
+export { filterData };
